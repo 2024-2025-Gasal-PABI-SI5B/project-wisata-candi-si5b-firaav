d782af4bd2e58b6f148a7cd8070e9641714f198f
firaav <alfirarahmadhina@gmail.com>
2024-11-07 09:44:02 +0700

    initial commit

--- candi_alfira/lib/main.dart
+++ candi_alfira/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:candi_alfira/screens/detail_screen.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +9,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Wisata Candi',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true  
       ),
+      home: DetailScreen(),
     );
   }
 }
