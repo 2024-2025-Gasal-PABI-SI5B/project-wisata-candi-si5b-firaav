add2c5115e2da00701f741d4787b7d9fc55019b0
firaav <alfirarahmadhina@gmail.com>
2024-11-18 07:07:28 +0700

    Update profile_screen.dart

--- candi_alfira/lib/screens/profile_screen.dart
+++ candi_alfira/lib/screens/profile_screen.dart
@@ -9,7 +9,11 @@ class ProfileScreen extends StatefulWidget {
   }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  //TODO: 1.deklarasikan vaariable
+  //TODO: 1.deklarasikan vaariable yang dibutuhkan
+  bool isSignedin = false;
+  String fullName = '';
+  String userName = '';
+  int favoriteCandiCount = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +24,33 @@ class _ProfileScreenState extends State<ProfileScreen> {
             width: double.infinity,
             color: Colors.deepPurple,
           ),
-          Column(
-            children: [],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                //TODO: 2.bagian profile header
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.deepPurple,
+                          width: 2,
+                        ),
+                        shape: BoxShape.circle),
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundImage: 
+                            AssetImage('images/placeholder_image.png'),
+                      )
+                    )
+                  ],
+                )
+                //TODO: 3. bagian profile info profil
+                //TODO: 4. Profile Action (tombol sign in dan out)
+            
+              ],
+            ),
           ),
         ],
       ),
