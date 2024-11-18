5a28b6d5f6f7b633bbc8abb2c695dbb4604285dc
firaav <alfirarahmadhina@gmail.com>
2024-11-18 08:59:46 +0700

    Update signin_screen.dart

--- candi_alfira/lib/screens/signin_screen.dart
+++ candi_alfira/lib/screens/signin_screen.dart
@@ -3,8 +3,22 @@ import 'package:flutter/material.dart';
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
 
+  //TODO:  1 dekklarasi  varible yang ddibutuhkan
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String _errorText = '';
+  bool _isSignIn = false;
+  bool _obscurePassword = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder()
+    return Scaffold();
+    //TODO 2: pasang border
+    appBar: AppBar(
+      title: const Text('Sign In'),
+    ),
+    //TODO 3: pasang body
+    body: Center();,
   }
 }
